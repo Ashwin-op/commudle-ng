7262a08fe6dfc5333d060736cba2ba6f8375f692
Arshdeep Singh <arsh199820@gmail.com>
2026-05-13 20:57:16 +0530

    refactor(api): remove deprecated user hackathons routes

--- libs/shared/services/src/lib/api-routes.constant.ts
+++ libs/shared/services/src/lib/api-routes.constant.ts
@@ -1005,11 +1005,6 @@ export const API_ROUTES = {
     HACKATHON_BULK_TEAM_GENERAL_EMAIL: 'api/v2/hackathon_user_response/hackathon_bulk_team_general_email', //POST
   },
 
-  USER_HACKATHONS: {
-    SPEAKER_JUDGE_MENTOR: 'api/v2/user_hackathons/speaker_judge_mentor', //GET
-    PARTICIPATED_AND_WON: 'api/v2/user_hackathons/participated_and_won', //GET
-  },
-
   ENTITY_UPDATES: {
     INDEX: 'api/v2/entity_updates',
     CREATE: 'api/v2/entity_updates',
